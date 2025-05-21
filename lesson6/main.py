f30a04048d5e4f32dfe5653a0698ede727a69938
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-05-21 09:12:52 -0700

    finished pokeapi

--- lesson6/main.py
+++ lesson6/main.py
@@ -16,61 +16,12 @@ import random
 # ---------- constants ----------
 POKE_COUNT = 1025                              # adjust if new pokemon come out (is this a thing?)
 API_URL    = "https://pokeapi.co/api/v2/pokemon/{}" # we will use {} later for substitution with .format()
-POKE_ID = random.randint(0,POKE_COUNT)
-
-# ---------- create our own dictionary of pokemon info ----------
-
-request = requests.get(API_URL.format(POKE_ID),timeout=10)
-request.raise_for_status()
-data = request.json()
-formatted_data = {
-    "id":           POKE_ID,
-    "name":         data["name"].title(),
-    "types":        [t["type"]["name"] for t in data["types"]], 
-    "weight_kg":    data["weight"]/10,
-    "sprite":       data["sprites"]["front_default"]
-    
-}
 
 # ---------- GUI Stuff ----------
-
 root = tk.Tk()
 root.title("Random Pokémon Generator")
 root.geometry("300x500")
 root.resizable(False, False)
-        
-img_bytes = requests.get(formatted_data["sprite"],timeout=10).content
-pillow_image = Image.open(io.BytesIO(img_bytes)).resize((200,200))
-tk_image = ImageTk.PhotoImage(pillow_image)
-
-"""
-    for widget in root.winfo_children():
-        # Check if the widget is a Label (ttk.Label or tk.Label)
-        if isinstance(widget, (tk.Label)):
-            widget.destroy()  # removes the label from the GUI
-    root.img_label = tk.Label()
-    root.img_label.pack(pady=(10, 5))
-    root.info_label = tk.Label(font=("Helvetica", 12), justify="center")
-    root.info_label.pack(pady=(0, 10))
-
-    root.btn = tk.Button(
-        text="Show Pokémon",
-        font=("Helvetica", 14, "bold"),
-        command = show_pokemon
-"""
-def show_pokemon(pokemon_id:int)->None:
-    try:
-        root.img_label.photo = fetch_image(API_URL, )
-        root.img_label.config(image=tk_image)
-
-        types = " / ".join(formatted_data["types"]).title() # convert dict to string
-        root.info_label.config(
-            text=f"{formatted_data['name']}  (#{POKE_ID})\n"
-                    f"Type: {types}\n"
-                    f"Weight: {formatted_data['weight_kg']} kg"
-        )
-    except Exception as e:
-        root.info_label.config(text=f"Error: {e}")
         
 def fetch_pokemon(pokemon_id: int) -> dict:
     request = requests.get(API_URL.format(pokemon_id),timeout=10)
@@ -83,15 +34,33 @@ def fetch_pokemon(pokemon_id: int) -> dict:
         "weight_kg":    data["weight"]/10,
         "sprite":       data["sprites"]["front_default"]
     }
+    return formatted_data
 
-def fetch_image(url: str, pokemon_id: int) -> ImageTk.PhotoImage:
-    img_bytes = requests.get(formatted_data["sprite"],timeout=10).content
+#pokemon_id is not needed
+def fetch_image(url: str) -> ImageTk.PhotoImage:
+    #url is the url of the sprite of the pokemon
+    img_bytes = requests.get(url,timeout=10).content
     pillow_image = Image.open(io.BytesIO(img_bytes)).resize((200,200))
     tk_image = ImageTk.PhotoImage(pillow_image)
     return tk_image
 
+def show_pokemon(pokemon_id:int)->None:
+    try:
+        pokemon_data = fetch_pokemon(pokemon_id)
+        #url of the sprite passed as argument to get image of pokemon
+        tk_image = fetch_image(pokemon_data["sprite"])
 
+        root.img_label.photo = tk_image
+        root.img_label.config(image=tk_image)
 
+        types = " / ".join(pokemon_data["types"]).title() # convert dict to string
+        root.info_label.config(
+            text=f"{pokemon_data['name']}  (#{pokemon_data['id']})\n"
+                    f"Type: {types}\n"
+                    f"Weight: {pokemon_data['weight_kg']} kg"
+        )
+    except Exception as e:
+        root.info_label.config(text=f"Error: {e}")
 
 # widgets
 root.img_label = tk.Label()
@@ -102,10 +71,9 @@ root.info_label.pack(pady=(0, 10))
 root.btn = tk.Button(
     text="Show Pokémon",
     font=("Helvetica", 14, "bold"),
-    command = show_pokemon
+    #lambda makes it so you can pass arguments to command without the parentheses making it run automatically
+    command = lambda : show_pokemon(random.randint(0,POKE_COUNT))
 )
 root.btn.pack(fill="x", padx=20, pady=5)
-
-
 
 root.mainloop()
